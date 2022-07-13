b223e1ae2d016c0417e4d992d1b47f0cd23a6503
patrick <patrickcotewurzler@yahoo.com>
2022-07-13 17:51:47 -0400

    patrick lab03 push

---
+++ code/patrick/python/lab03.py
@@ -0,0 +1,13 @@
+total_amount = (input("\nEnter the total amount "))
+
+converted_total_amount =  int(round(float(total_amount)*100))
+
+print(converted_total_amount//25, "quarters")
+converted_total_amont = converted_total_amount%25
+print(converted_total_amount//10, "dimes")
+converted_total_amount = converted_total_amount%10
+print(converted_total_amount//5, "nickles")
+converted_total_amount = converted_total_amount%5
+
+
+
