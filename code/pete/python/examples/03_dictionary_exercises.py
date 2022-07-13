6fdf9a8056d1933d4562edf718b7cd73beb2ace7
pjz987 <pwj2012@gmail.com>
2022-07-13 10:09:55 -0700

    dictionaries prep

---
+++ code/pete/python/examples/03_dictionary_exercises.py
@@ -0,0 +1,45 @@
+miles_morales = {
+    'superhero': 'Spider-Man',
+    'age': 17,
+    'home': 'New York City',
+    'family': ['Jefferson Davis', 'Rio Morales'],
+    'occupation': 'student',
+}
+
+"""
+Access: dict[key] or dict.get(key)
+Access the 'superhero' key to print Miles' superhero name
+"""
+
+
+"""
+Update: dict[key] = value
+Happy Birthday Miles 🎂
+Increase Miles' age by 1
+"""
+
+
+
+"""
+Add Miles' uncle, Aaron Davis, to the 'family' list value
+"""
+
+
+
+"""
+Add key/value pair: dict[key] = value
+Add more info to the miles_morales dictionary
+"""
+
+
+
+"""dict.keys()"""
+
+
+"""dict.values()"""
+
+
+
+"""dict.items()"""
+
+
