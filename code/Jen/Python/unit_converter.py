ece0fb5370d9b4fba2897ed88fed60659272be2c
NSJen <williamsjena@outlook.com>
2022-07-13 18:47:23 -0400

    Revisions to lab 1

--- code/Jen/Python/unit_converter.py
+++ code/Jen/Python/unit_converter.py
@@ -1,7 +1,7 @@
 '''Jen Williams
 PDX Code Guild Bootcamp
-lab 1: Unit Converter'''
-
+lab 1: Unit Converter
+Revision 1'''
 
 
 
@@ -9,41 +9,43 @@ lab 1: Unit Converter'''
 distance = input("What is the distance? ")
 unitin = input("What are the input units? Choose mi for miles, ft for feet, or m for meters. ")
 unitout = input("What would you like to convert to? Choose mi for miles, ft for feet, m for meter or km for kilometer. ")
+distance = int(distance)
 
-if unitin == unitout: 
-    x = 1
-
-elif unitin == "mi": 
-    if unitout == "m":
-        x = 1609.34
-    elif unitout == "km":
-        x = 1.60934
-    elif unitout == "ft":
-        x = 1609.34 * 1/0.348
+if unitin == "mi": 
+    meterdistance = distance * 1609.34
 
 elif unitin == "ft":
-    if unitout == "m":
-        x = 0.3048
-    elif unitout == "km":
-        x = .0003048
-    elif unitout == "mi":
-        x = .3048 * 1/1609.34
+    meterdistance = distance * .3048
 
 elif unitin == "m":
-    if unitout == "mi":
-        x = 1/1609.34
-    elif unitout == "ft":
-        x = 1/0.3048
-    elif unitout == "km":
-        x = 1000
+    meterdistance = distance * 1
+
+elif unitin == "km":
+    meterdistance = distance * 1000
+
 else:
     print("You have chosen an invalid input. Please choose from the available options")
     quit()
 
-distance = float(distance)
-output = round(distance * x, 4)
+#distance = float(distance)
+#output = round(distance * x, 4)
+
+if unitout == "mi":
+    output = meterdistance * 1/1609.334
+
+elif unitout == "ft":
+    output = meterdistance * 1/0.3048
+
+elif unitout == "m":
+    output = meterdistance * 1
+
+elif unitout == "km":
+    output = meterdistance * 1/1000
+
+output = round(output , 4)
 
 print (f"{distance} {unitin} = {output} {unitout}")
+
 
 
 
