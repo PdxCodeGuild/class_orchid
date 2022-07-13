e24ed59dfbffeaded4c43d0a30ef5ebdef695e5e
mileidy <mmileidy015@gmail.com>
2022-07-13 18:55:26 -0500

    saving progress

---
+++ code/mileidy/python/lab03_version1.py
@@ -0,0 +1,14 @@
+
+converter ={
+    'pennies': 100
+}
+
+dollar_amount = input('Please enter the total dollar amount: ')
+
+dollar_amount = int(dollar_amount)
+
+f_conversion = dollar_amount * converter['pennies'] 
+
+s_conversion = f_conversion // 
+
+
