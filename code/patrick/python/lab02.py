7f94866871ce345040613d38975c0c8e987fb46b
patrick <patrickcotewurzler@yahoo.com>
2022-07-13 18:08:27 -0400

    patrick lab02 commit

--- code/patrick/python/lab02.py
+++ code/patrick/python/lab02.py
@@ -1,20 +1,4 @@
-#nums = [5,0,8,4,1,6,] #part1
-
-
-nums = []
-total = 0
-
-while True:
-    user_responce = input("\nPlease enter a number or 'done' to calcuate")
-    if user_responce == "done":
-        break
-    else:
-        nums.append(int(user_responce))
-    
-
-#nums = int(nums)
-for item in nums:
-total += user_responce
+nums = [5,0,8,4,1,6,] #part1
 
 
 lenght_of_list = len(nums)#part1
