fa28580efba5060b7b8c4d745446780d7d437377
Velshaun <vellbyers@gmail.com>
2022-07-14 11:59:34 -0500

    Lab02 added version 1

--- code/vel/python/02lab.py
+++ code/vel/python/02lab.py
@@ -26,14 +26,14 @@ print(average(nums))
 
 #### Version 2 ####
 
-# numbers = []
-# while True:
-#     nums = input('\nEnter your number here, if done type done: ')
-#     if nums == "done":
-#         print(f"Average: {sum(numbers) / len(numbers)}")
-#         break
-#     nums = int(nums)
-#     numbers.append(nums)
+numbers = []
+while True:
+    nums = input('\nEnter your number here, if done type done: ')
+    if nums == "done":
+        print(f"Average: {sum(numbers) / len(numbers)}")
+        break
+    nums = int(nums)
+    numbers.append(nums)
 
 # print(numbers) for testing purposes 
 
