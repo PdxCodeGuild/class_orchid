344d9e8a6f9c64eb185578e41d5b50931ce387f0
patrick <patrickcotewurzler@yahoo.com>
2022-07-14 15:23:09 -0400

    lab05 commit

--- code/patrick/python/lab05.py
+++ code/patrick/python/lab05.py
@@ -12,18 +12,18 @@ cards = {
      "queen" : 10,
      "king" : 10
   }
+  
+  
+users_card1 = input("\nEnter the first card ")
+users_card2 = input("\nEnter the second card ")
+users_card3 = input("\nEnter the third card ")
 
- users_card1 = input("\nEnter the first card ")
- users_card2 = input("\nEnter the second card ")
- users_card3 = input("\nEnter the third card ")
+print(cards[users_card1])
+print(cards[users_card2])
+print(cards[users_card3])
+cards_played = cards[users_card1] + cards[users_card2] + cards[users_card3]
 
- print(cards[users_card1])
- print(cards[users_card2])
- print(cards[users_card3])
-
- cards_played = cards[users_card1] + cards[users_card2] + cards[users_card3]
-
- if cards_played <= 17:
+if cards_played <= 17:
      print("Hit")
- if cards_played >= 18:
-      print("stay")
+if cards_played >= 18:
+        print("stay")
