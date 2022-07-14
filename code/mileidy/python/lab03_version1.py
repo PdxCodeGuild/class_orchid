bf0d892786f4e39794da0f20f767316dd081cb6a
mileidy <mmileidy015@gmail.com>
2022-07-13 20:03:54 -0500

    completed lab 3 version 1

--- code/mileidy/python/lab03_version1.py
+++ code/mileidy/python/lab03_version1.py
@@ -1,14 +1,23 @@
-
-converter ={
-    'pennies': 100
-}
-
 dollar_amount = input('Please enter the total dollar amount: ')
 
-dollar_amount = int(dollar_amount)
+dollar_amount = float(dollar_amount)
 
-f_conversion = dollar_amount * converter['pennies'] 
+quarters = dollar_amount // 0.25
+#print(quarters)
 
-s_conversion = f_conversion // 
+dollar_amount -= quarters * .25 
+#print(dollar_amount)
 
+dimes = dollar_amount // 0.10
 
+dollar_amount -= dimes * 0.10
+
+nickles = dollar_amount // 0.05
+
+dollar_amount -= nickles * 0.05
+
+pennies = dollar_amount // 0.01
+
+dollar_amount -= pennies * 0.01
+
+print(f'{quarters} quarters, {dimes} dimes, {nickles} nickles, {pennies} pennies.')
