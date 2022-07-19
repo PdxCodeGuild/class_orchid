25826b37982601b607b05f097d69e7741270f38c
NSJen <williamsjena@outlook.com>
2022-07-19 18:22:10 -0400

    changed formulas

--- code/Jen/Python/converter_new.py
+++ code/Jen/Python/converter_new.py
@@ -13,20 +13,18 @@ metconvfromm = {
     'y' : 1.0936133,
     'm' : 1,
     'km' : 1/1000,
-    'mi' : 1/609.34,
+    'mi' : 1/1609.34,
 }
 
-distance = input("What is the distance you would like converted? ")
+initial_distance = input("What is the distance you would like converted? ")
 unitsfrom = input("What unit of measurement are you converting from? Please choose in, ft, m, y, km, or mi. ")
 unitsto = input("What unit of measurement are you converting to? Please choose in, ft, m, y, km, mi ")
 
 start = float(metconvtom.get(unitsfrom))
 to = float(metconvfromm.get(unitsto))
 
-initconvert = float(distance) * start
+initconvert = float(initial_distance) * start
 
-finalconvert = float(initconvert) * to
+distance = float(initconvert) * to
 
-distance = round(start * to , 4)
-
-print(distance)
+print(f"{initial_distance} {unitsfrom} = {round(distance, 4)} {unitsto}")
