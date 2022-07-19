3935c5350cde5367e7e80357e6d2a04d2f4a0bb9
Velshaun <vellbyers@gmail.com>
2022-07-19 12:55:44 -0500

    lab07

---
+++ code/mobs/jack_of_all/lab07.py
@@ -0,0 +1,12 @@
+"""
+Lab 07: Jackalope Simulator -- Mob
+"""
+
+jackalopes = [
+    {
+        "id": 1,
+        "age": 0,
+        "is pregnant": False,
+        "off-spring counter": 0,
+    },
+]
