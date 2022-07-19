6ba6f07403db21ec5c3db374be30de8de3bdb0ef
JordynHolder <Jordyn.W.Holder@gmail.com>
2022-07-19 10:45:48 -0700

    jordyn edits

--- code/mobs/jackalopez/lab07.py
+++ code/mobs/jackalopez/lab07.py
@@ -1,12 +1,17 @@
-jackalopes = [ 2, 1, ]
+jackalopes = [ 2, 10, ]
 
-def one_year(jackalopes):
+def one_year(): #adds 1 year to life
     for index,age in enumerate(jackalopes):
         jackalopes[index] = age +1
         
+def prune():
+    for index,age in enumerate(jackalopes):
+        if age >= 10:
+            jackalopes.pop(index)
 
 
-one_year(jackalopes)
+one_year()
+prune()
 
 
 print(jackalopes)    
