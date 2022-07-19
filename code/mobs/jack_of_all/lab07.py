50242758d8375d9cd013e5479e56e2cfe5b68017
robrpete <rob.r.pete@gmail.com>
2022-07-19 15:26:35 -0400

    jack_of_all lab07 finished

--- code/mobs/jack_of_all/lab07.py
+++ code/mobs/jack_of_all/lab07.py
@@ -2,11 +2,28 @@
 Lab 07: Jackalope Simulator -- Mob
 """
 
-jackalopes = [
-    {
-        "id": 1,
-        "age": 0,
-        "is pregnant": False,
-        "off-spring counter": 0,
-    },
-]
+jackalopes = [0,0]
+population = 0
+year = 0
+
+while population <= 1000:
+    population = len(jackalopes)
+
+    #increase by 1
+    for index, jack in enumerate(jackalopes):
+        mate_check = index + 1
+        jackalopes[index] += 1
+    #check if age >= 4 and <= 8
+        if jack >= 4 and jack <= 8 and (index % 2) == 0:
+            jackalopes.extend([0,0])
+        
+    while 10 in jackalopes:
+        for index, jack in enumerate(jackalopes):
+            if jack == 10:
+                jackalopes.pop(index)
+    
+        
+
+    year += 1
+    print('year: ', year,'population: ', population)
+    # print(jackalopes)
