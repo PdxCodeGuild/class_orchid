9fd5ec4b1b675b5802be8714da2b05b16b882c85
NSJen <williamsjena@outlook.com>
2022-07-19 15:31:07 -0400

    lab07

--- code/mobs/07lab.py
+++ code/mobs/07lab.py
@@ -3,17 +3,49 @@
 # 1 gestational pair = 2 offspring per year / cycles every +2 to population every four years// 
 # 2 die every 10 years
 
-
+"""
 
 population = 2
 
 #yr_1 through yr_3 nothing happens for first pair
 
 time = 0
-years = []
-birth_rate = [ ('group_0'), 
+years = {
+
+    #'born': 0
+    #'died' : 0
+   # 'population' : 2
+
+}
+
+births = {
+    'age 0' = 
+    'age 1' =
+    'age_2' =
+    'age_3' =
+}
+
+
+
+if years == range(1, 4):
+
+elif years == range(4, 9):
+    run formula for 2 babies each year
+
+elif years == range(9-11):
+    run formula for exponential babies, start elderly
+
+elif years == 10:
+    start death, exponential babies, elderly
+
+elif years > 12:
+    run fomula for death, exponential babies, exponential elderly
+
+
+age = [ ('group_0'), 
     ('group_2')
 ]
+
 offspring =[]
 
 def growth(time, population):
@@ -23,9 +55,10 @@ def growth(time, population):
         time += 1
         years.append(time)
         offspring.append(population)
+
     return offspring
 
-print(growth(2, 2))
+print(growth(time, population))
 print(population)
 print(time)
 #for _ range(5):
@@ -34,7 +67,7 @@ print(time)
 # population * (1 + 100%) ** time
 
 #breeding age formula added for population at year five 
-"""
+
 while population < 1000:
     run the growth function
     run the death function
@@ -58,3 +91,19 @@ add 4 on the end | how long these two will take to make 1000 babies
 
 
 """
+
+pop_by_age = [0,0,0,0,2] #don't forget the first 3 years
+total_pop = sum(pop_by_age)
+ #still need to work deaths in
+for age in pop_by_age:
+    # enumerate function can recall index and value
+    # need to be able to recall breeding age animals and add them together
+    
+    print(age)
+    
+
+
+#nonbreeding_pop =
+#breeding_pop = 
+
+#.insert
