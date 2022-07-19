3bca8497db655a918a932c4d7d5409b403ee739d
JordynHolder <Jordyn.W.Holder@gmail.com>
2022-07-19 10:36:36 -0700

    moved to mob

---
+++ code/mobs/jackalopez/lab07.py
@@ -0,0 +1,13 @@
+jackalopes = [ 2, 1, ]
+
+def one_year(jackalopes):
+    for index,age in enumerate(jackalopes):
+        jackalopes[index] = age +1
+        
+
+
+one_year(jackalopes)
+
+
+print(jackalopes)    
+
