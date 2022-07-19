ecb687ad1dd01581ff0158d25f47165440aabb95
JordynHolder <Jordyn.W.Holder@gmail.com>
2022-07-19 13:41:44 -0700

    update

--- code/mobs/jackalopez/lab07.py
+++ code/mobs/jackalopez/lab07.py
@@ -1,34 +1,38 @@
-jackalopes = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+import math
+
+jackalopes = [ 0, 0]
             
 def one_year(index,age): #adds 1 year
     jackalopes[index] = age + 1
 
-def prune(index,age): #removes the old
-    if age >= 10:
-        jackalopes.pop(index)
-
 def birth(birthing_age,age): #finds population capable of birth
+    age += 1
     if age in range(4,9):
         return birthing_age + 1 
     else:
         return birthing_age
 
 birthing_age = 0
+new_pop = 1
+year = 1
 
-# while len(jackalopes) <= 1000:
-for index,age in enumerate(jackalopes):
-    one_year(index,age)
-    prune(index,age)
-    birthing_age = birth(birthing_age,age)
+while len(jackalopes) <= 1000: #population loop
+    birthing_age = 0
+    new_pop = 1
+    for index,age in enumerate(jackalopes): #finds population capable of birth
+        one_year(index,age)
+        birthing_age = birth(birthing_age,age)
 
-'''
-birthing_age // 2 -------- Gets a whole number for the new population loop
-'''
+    while 10 in jackalopes: #removes all at age 10
+        for index,age in enumerate(jackalopes):
+            if age >= 10:
+                jackalopes.pop(index)
+
+    while new_pop <= birthing_age: #adds new population to end of list
+        jackalopes.append(0)
+        new_pop += 1
+
+    year += 1
 
 
-
-# print(birthing_age)
-
-
-print(jackalopes)    
-
+print(year)
