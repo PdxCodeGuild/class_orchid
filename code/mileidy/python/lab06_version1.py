54022acad536ceb51c93bf2e5060ebb8ed51c767
mileidy <mmileidy015@gmail.com>
2022-07-20 14:12:53 -0500

    saving progress

--- code/mileidy/python/lab06_version1.py
+++ code/mileidy/python/lab06_version1.py
@@ -1,17 +1,5 @@
 import random
 
-numbers_list = []
+def pick6():
+    random.randint
 
-def pick6(6, low=1, high=99):
-  
-    numbers_list = []
-
-    for x in range(6):
-        random_number = random.randint(low, high)
-
-
-    numbers.append(random)
-
-numbers = pick6(6, 1, 99)
-
-print(numbers)
