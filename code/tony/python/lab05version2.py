1a6ae1b6a6ae211756bf591fcc8617067df8fa23
Tony <tony@webavant.com>
2022-07-20 11:08:53 -0700

    refactoring as per review and in general

--- code/tony/python/lab05version2.py
+++ code/tony/python/lab05version2.py
@@ -2,35 +2,36 @@
 accepted = [ 'a', 'j', 'q', 'k', '2', '3', '4', '5', '6', '7', '8', '9', '10' ]
 
 # Translation values for face card names and loop iterations
-values = { 'a': 11, 'j': 10, 'q': 10, 'k': 10, }
-iteration_string = ['first','second','third']
+translations = { 'a': 1, 'j': 10, 'q': 10, 'k': 10, }
 
-# Initialize values
-totals = [0]
-i = 0
+def translate(val):
+    '''Translate user input.'''
+    try:
+        val = int(val)
+    except ValueError:
+        val = translations[val]
+    return val
+
+# initialize total
+total = 0
+aces = 0
 
 # Input three cards
-while i < 3:
-    val = input(f'What\'s your {iteration_string[i]} card? ').lower()
-    if not val in accepted:
+while True:
+    val = input(f'Enter which card you drew: ').lower()
+    if val in accepted:
+        if val == 'a': aces += 1
+        val = translate(val)
+        total += val
+        if total == 21 or total + aces * 10 == 21 or total + (aces - 1) * 10 == 21:
+            print(f'{total} Blackjack!')
+            break
+        if total < 7:
+            print(f'{total} Hit')
+        elif total >= 7 and total < 21:
+            print(f'{total} Stay')
+        elif total > 21 :
+            print(f'{total} Busted!')
+            break
+    else:
         print('Accepted values: a, j, q, k, [2-10]')
-        continue
-    if val in values.keys():
-        val = values.get(val)
-    i += 1
-    totals[0] += int(val)
-    if val == 11 and len(totals) < 2:
-        totals.append(totals[0])
-    if val == 11:
-        totals[1] += 1
-
-# Give blackjack advice
-for total in totals:
-    if total < 17:
-        print(f'{total} Hit')
-    elif total >= 17 and total < 21:
-        print(f'{total} Stay')
-    elif total == 21:
-        print(f'{total} Blackjack!')
-    elif total > 21:
-        print(f'{total} Already Busted!')
