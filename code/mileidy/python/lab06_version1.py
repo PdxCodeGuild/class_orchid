a216f400750efad39c4785e23d2e6873719357ac
mileidy <mmileidy015@gmail.com>
2022-07-20 21:44:13 -0500

    saving progress on lab 06

--- code/mileidy/python/lab06_version1.py
+++ code/mileidy/python/lab06_version1.py
@@ -1,5 +1,19 @@
 import random
 
-def pick6():
-    random.randint
+def pick6(k, low = 1, high = 99): #referred back to my 102 notes
+    numbers_list = []
+    for x in range(6):
+        random_number = random.randint(low, high)
+        numbers_list.append(random_number)
+    return numbers_list
 
+numbers_list = pick6(6, 1, 99) 
+# print(numbers_list) succesfully got 6 random numbers in range of 1-99
+winning_ticket = pick6()
+balance = 0
+
+def num_matches(winning_ticket, ticket):
+    for num in winning_ticket == num in ticket:
+        return num
+    
+for x in range(100000):
