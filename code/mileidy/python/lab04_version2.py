86c44b31b92beee84ee85450cda8df2054a0a3a3
mileidy <mmileidy015@gmail.com>
2022-07-21 13:30:33 -0500

    updated lab04 complete

--- code/mileidy/python/lab04_version2.py
+++ code/mileidy/python/lab04_version2.py
@@ -1,7 +1,7 @@
 print('Hello and Welcome!')
 
 number = input('Please enter the numbers you would like converted: ')
- 
+
 number = int(number)
 
 hundreds_digit = number//100
@@ -11,41 +11,41 @@ tens_digit = number//10 %10
 ones_digit = number%10
 
 ones = {
-     0 : 'zero',
-     1 : 'one',
-     2 : 'two',
-     3 : 'three',
-     4 : 'four',
-     5 : 'five',
-     6 : 'six',
-     7 : 'seven',
-     8 : 'eight',
-     9 : 'nine',
+    0 : 'zero',
+    1 : 'one',
+    2 : 'two',
+    3 : 'three',
+    4 : 'four',
+    5 : 'five',
+    6 : 'six',
+    7 : 'seven',
+    8 : 'eight',
+    9 : 'nine',
 }
 
 teens = {
-     1 : 'eleven',
-     2 : 'twelve',
-     3 : 'thirteen',
-     4 : 'fourteen',
-     5 : 'fifteeen',
-     6 : 'sixteen',
-     7 : 'seventeen',
-     8 : 'eighteen',
-     9 : 'nineteen'
+    1 : 'eleven',
+    2 : 'twelve',
+    3 : 'thirteen',
+    4 : 'fourteen',
+    5 : 'fifteeen',
+    6 : 'sixteen',
+    7 : 'seventeen',
+    8 : 'eighteen',
+    9 : 'nineteen'
 }
 
 tens = {
-     0 : '',
-     1 : 'ten',
-     2 : 'twenty',
-     3 : 'thirty',
-     4 : 'fourty',
-     5 : 'fifty',
-     6 : 'sixty',
-     7 : 'seventy',
-     8 : 'eighty',
-     9 : 'ninety'
+    0 : '',
+    1 : 'ten',
+    2 : 'twenty',
+    3 : 'thirty',
+    4 : 'fourty',
+    5 : 'fifty',
+    6 : 'sixty',
+    7 : 'seventy',
+    8 : 'eighty',
+    9 : 'ninety'
 }
 
 hundreds = {
@@ -89,9 +89,9 @@ if hundreds_digit == 0:
 else:
 
     #check if number has a teen above 99.
-    if tens_digit == 1 and ones_digit <= 9: #teens 11-19 conversion
+    if tens_digit == 1 and ones_digit > 0: #teens 11-19 conversion
         teens_string = teens[ones_digit]
-
+        hundreds_string = hundreds[hundreds_digit]
 
     else:
         hundreds_string = hundreds[hundreds_digit]
@@ -100,18 +100,19 @@ else:
 
         hundreds_string = hundreds[hundreds_digit]
 
-        if ones_string == 'zero':
-            ones_string = ''
-            
-            if tens_string != '':
-                hundreds_string += ' '
-
-        elif tens_string == '':
+    if ones_string == 'zero':
+        ones_string = ''
+        
+        if tens_string != '':
             hundreds_string += ' '
+            
+    elif tens_string == '':
+        hundreds_string += ' '
 
-        else:
-            tens_string +=  ' '
+    else:
+        tens_string +=  ' '
+        hundreds_string += ' '
 
-nums_String = hundreds_string + teens_string + " " +  tens_string + ones_string
+nums_String = hundreds_string + teens_string + tens_string + ones_string
 
 print(f"Your number is {nums_String}")
