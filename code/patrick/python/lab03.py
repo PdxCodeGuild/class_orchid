41deab4ec2d75871a8891d98795760ab71b4f7d8
patrick <patrickcotewurzler@yahoo.com>
2022-07-21 17:58:09 -0400

    lab03 final

--- code/patrick/python/lab03.py
+++ code/patrick/python/lab03.py
@@ -3,12 +3,12 @@ total_amount = (input("\nEnter the total amount "))
 converted_total_amount =  int(round(float(total_amount)*100))
 
 print(converted_total_amount//25, "quarters")
-converted_total_amont = converted_total_amount%25
-print(converted_total_amount//100, "dimes")
+converted_total_amount = converted_total_amount%25
+print(converted_total_amount//10, "dimes")
 converted_total_amount = converted_total_amount%10
 print(converted_total_amount//5, "nickles")
 converted_total_amount = converted_total_amount%5
 print(converted_total_amount//1, "pennies")
-converted_total_amount = converted_total_amount%1
+converted_total_amount = converted_total_amount%1 
 
 
