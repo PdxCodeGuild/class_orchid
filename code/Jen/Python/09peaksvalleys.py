4e952606246a5f104660cb86eae5783da4623167
NSJen <williamsjena@outlook.com>
2022-07-22 19:15:55 -0400

    changed to three functions.

--- code/Jen/Python/09peaksvalleys.py
+++ code/Jen/Python/09peaksvalleys.py
@@ -5,26 +5,45 @@ Class Orchid
 lab 9: Peaks and Valleys'''
 
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
-peaks = []
-valleys = []
+peaks_list = []
+valleys_list = []
+
+def peaks(i, number, number_before, number_after):
+    if number_before < number > number_after:
+        return i
+    else:
+        i = 0
+        return i
+ 
 
 
-for index in range(1,20):
-    number_before = data[index-1]
-    number_after = data[index+1]
-    if number_before < data[index] > number_after:
-        peaks.append(index)
-    elif number_before > data[index] < number_after:  
-        valleys.append(index)
+def valleys(i, number, number_before, number_after):
+    if number_before > number < number_after:
+        return i
+    else:
+        i = 0
+        return i
 
 
-print(f"Peaks = {peaks}")
-print(f"Valleys = {valleys}")
-peaks.extend(valleys)
-peaks.sort()
-print(f"Peaks and valleys = {peaks}")
+def peaks_valleys(peaks_list, valleys_list):
+    peaks_list.extend(valleys_list)
+    peaks_list.sort()
+    consolidated = peaks_list
+    return consolidated
+
+
+for i, number in enumerate(data):
+    if i in range(1,19):
+        #print(data)
+        peak = peaks(i, number, data[i-1], data[i+1])
+        if peak:
+            peaks_list.append(i)
         
+        valley = valleys(i, number, data [i-1], data[i+1])
+        if valley:
+            valleys_list.append(i)
 
 
-
-
+print(f"Peaks = {peaks_list}")
+print(f"Valleys = {valleys_list}")
+print(f"Peaks and valleys = {peaks_valleys(peaks_list, valleys_list)}")
