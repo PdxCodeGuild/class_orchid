2c2e3cb8c004fcb3e61c635de9b86847197c57b6
robrpete <rob.r.pete@gmail.com>
2022-07-25 16:56:22 -0400

    fix print statements

--- code/rob/python/lab10.py
+++ code/rob/python/lab10.py
@@ -1,9 +1,7 @@
 english = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 rot =     ['n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
 
-user_input = input('Enter a word to ROT+13: ')
-
-def rotate_word(user_input):
+def encryp_word(user_input):
     encryp =''
     for char in user_input:
         if char == ' ':
@@ -13,6 +11,27 @@ def rotate_word(user_input):
             encryp += rot[index]
     return encryp
 
-user_input_encryp = rotate_word(user_input)
+def decryp_word(user_input):
+    decryp =''
+    for char in user_input:
+        if char == ' ':
+            decryp += char
+        else:
+            index = rot.index(char)
+            decryp += english[index]
+    return decryp
 
-print(user_input_encryp)
+user_input = '0'
+
+while user_input != '':
+    print('\nPress enter to exit')
+    user_input = input('Enter a word to encryp: ')
+    if user_input == '': break
+    user_input_encryp = encryp_word(user_input)
+    print('Encryp: ', user_input_encryp)
+    
+    user_input = input('Enter a word to decryp: ')
+    user_input_decryp = decryp_word(user_input)
+    print('Decryp: ', user_input_decryp)
+
+print('bye\n')
