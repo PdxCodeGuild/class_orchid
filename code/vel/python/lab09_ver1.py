cea7cf8978a31166de52a566eb5d3034cc7d9b92
Velshaun <vellbyers@gmail.com>
2022-07-25 18:54:30 -0500

    lab09 fixed

--- code/vel/python/lab09_ver1.py
+++ code/vel/python/lab09_ver1.py
@@ -7,7 +7,7 @@ def find_peaks():
     peaks = []
     for i in range(1, len(data)-1): # range(start, stop, step) start = 1
         if data[i -1] < data[i] and data[i + 1] < data[i]:
-            peaks.append(data[i])
+            peaks.append(i)
     return peaks
 
 #IndexError index out of range
@@ -16,17 +16,21 @@ def find_valleys():
     valleys = []
     for i in range(1, len(data)-1):
         if data[i -1] > data[i] and data[i + 1] > data[i]:
-            valleys.append(data[i])
+            valleys.append(i)
     return valleys
 
-def peaks_valleys():
-    peaks_and_valleys = []
-    peaks_and_valleys.extend(find_valleys()) # function object is not iterable, '()' makes that function iterable.
-    peaks_and_valleys.extend(find_peaks())
-    peaks_and_valleys.sort()
-    return peaks_and_valleys
+def peaks_valleys(lst):
+    peaks = find_peaks()
+    valleys = find_valleys()
+    peaks.extend(valleys)
+    peaks.sort()
+    return peaks
 
-print(peaks_valleys())
+print(f'peaks > {find_peaks()}')
+
+print(f'\nvalleys >{find_valleys()}\n')
+
+print(f'peaks and valleys >{peaks_valleys(data)}\n')
 
 
 
