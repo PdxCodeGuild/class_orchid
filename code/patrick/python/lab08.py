5b0db8dd407eda25dada6d15ff027d3b9ae9c562
patrick <patrickcotewurzler@yahoo.com>
2022-07-26 19:03:31 -0400

    lab08 in progress

--- code/patrick/python/lab08.py
+++ code/patrick/python/lab08.py
@@ -26,14 +26,24 @@ def convert_string(string):
 #print(check_digi)
 converted_string = convert_string(card_number) 
 
-print(converted_string)
+#print(converted_string)
 
-check_number = converted_string[15:16]   
-    
+   
+check_number = converted_string.pop(15)
 
 print(check_number)
-#print(type(converted_string))
+
 #print(converted_string)
-print(converted_string)
+
+reversed_string = converted_string[::-1]
+
+print(reversed_string)
+
+mulitplied_list = reversed_string[: ]
+mulitplied_list[::2] = [x*2 for x in mulitplied_list[::2]]
+
+
+print(mulitplied_list)
+
 
 
