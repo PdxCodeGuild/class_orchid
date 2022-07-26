c0b8fbc9f333e769f65011143114e0584b8e4f77
patrick <patrickcotewurzler@yahoo.com>
2022-07-26 16:37:41 -0400

    lab08 in progress

--- code/patrick/python/lab08.py
+++ code/patrick/python/lab08.py
@@ -1,52 +1,39 @@
-from pyparsing import nums
+
 
 
 print(f"\nWelcome to credit card validation")
 
 card_number = input(f"\nPlease enter your card number for validation ")
 
-
-
+nums = []
+converted_string = []
 def convert_string(string):
     converted_card_number = list(string)
-    
+    return converted_card_number
     #return converted_card_number
     #print(converted_card_number)
-    for item in converted_card_number:
-        nums = []
-        nums = item.split(",")
-        print(nums[0])
-        #check_number = nums[-16:0:1]
-        #check_number = nums.pop(-1)    
-    print(nums)
-    #print(nums)
-    return nums
     
-    #print(type(converted_card_number))
+    
+    #
 
 
+
+
+#print(nums)
 ##  4556737586899855
-
-print(nums)
-
+#check_digi = nums
+#print(nums)
+#print(check_digi)
 converted_string = convert_string(card_number) 
 
+print(converted_string)
 
+check_number = converted_string[15:16]   
+    
 
-print(type(converted_string))
+print(check_number)
+#print(type(converted_string))
+#print(converted_string)
 print(converted_string)
 
 
-
-#converted_string = converted_string
-#converted_string.s
-
-#print(converted_string[::16])
-
-# card_number = int(card_number)
-
-#check_number = converted_string.pop((-1))
-
-#print(converted_string)
-#print(check_number)
-#print(check_number)
