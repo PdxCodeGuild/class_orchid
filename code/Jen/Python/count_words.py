93c946a9ea97f30d18fae0ce7b47e97713c62faa
NSJen <williamsjena@outlook.com>
2022-07-27 19:20:04 -0400

    Lab:21: Count Words

---
+++ code/Jen/Python/count_words.py
@@ -0,0 +1,40 @@
+'''Jen Williams
+PDX Code Guild Bootcamp - Orchid
+lab 21: Count Words - Optional'''
+
+import string
+
+with open('hackers.txt', 'r', encoding='utf-8') as f:
+    contents = f.read()
+    f.close()
+
+
+lucy = str(contents.lower())
+translator = str.maketrans('', '', string.punctuation)
+lucy_without_punct = lucy.translate(translator)
+
+
+lucy_dict = {}
+lucy_list = lucy_without_punct.split()
+
+
+for items in lucy_list:
+    if items in lucy_dict:
+        lucy_dict[items] += 1
+    else: 
+        lucy_dict[items] = 1 
+
+bad_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+for bad in bad_words:
+    if bad in lucy_dict:
+        del lucy_dict[bad]
+
+words = list(lucy_dict.items()) 
+
+words.sort(key=lambda tup: tup[1], reverse=True)  
+
+
+
+for i in range(min(10, len(words))):  
+    print(words[i])
+
