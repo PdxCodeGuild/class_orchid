648fc91294cdcdff20dcdf396820afe5d8870014
patrick <patrickcotewurzler@yahoo.com>
2022-07-27 13:03:03 -0400

    lab08 ready for grading

--- code/patrick/python/lab08.py
+++ code/patrick/python/lab08.py
@@ -10,20 +10,9 @@ converted_string = []
 def convert_string(string):
     converted_card_number = list(string)
     return converted_card_number
-    #return converted_card_number
-    #print(converted_card_number)
+   
     
     
-    #
-
-
-
-
-#print(nums)
-##  4556737586899855
-#check_digi = nums
-#print(nums)
-#print(check_digi)
 converted_string = convert_string(card_number) 
 
 #print(converted_string)
@@ -31,19 +20,59 @@ converted_string = convert_string(card_number)
    
 check_number = converted_string.pop(15)
 
-print(check_number)
+#print(check_number)
 
 #print(converted_string)
 
 reversed_string = converted_string[::-1]
 
-print(reversed_string)
+#print(reversed_string)
 
 mulitplied_list = reversed_string[: ]
-mulitplied_list[::2] = [x*2 for x in mulitplied_list[::2]]
+mulitplied_list[::2] = [int(x)*2 for x in mulitplied_list[::2]]
 
 
-print(mulitplied_list)
 
+#print(mulitplied_list)
+
+mulitplied_list = list(mulitplied_list)
+
+#print(type(mulitplied_list))
+
+multiplied_list_int = []
+for item in mulitplied_list:
+    item = int(item)
+    multiplied_list_int.append(item)
+
+#print(multiplied_list_int)
+
+minus_9_list = []
+for item in multiplied_list_int:
+    if item <= 9:
+        minus_9_list.append(item)
+    if item >= 9:
+        item = item - 9
+        minus_9_list.append(item)
+        
+#print(minus_9_list)
+
+added_list = sum(minus_9_list)
+
+int_check_num = int(check_number)
+#print(type(int_check_num))
+#print(added_list)
+
+changed_added_list = added_list % 10
+
+#print(changed_added_list)
+#print(type(changed_added_list))
+#print(int_check_num)
+#print(type(int_check_num))
+
+
+if changed_added_list == int_check_num:
+    print("verification Successful")
+if changed_added_list != int_check_num:
+    print("unsuccessful")
 
 
