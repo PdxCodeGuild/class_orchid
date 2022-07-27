2a09d13f5dfb9a83d777416bc23917638d283a68
NSJen <williamsjena@outlook.com>
2022-07-27 16:40:54 -0400

    added options for spaces and capital letters as well as handling for punctuation.

--- code/Jen/Python/10_rotcipher.py
+++ code/Jen/Python/10_rotcipher.py
@@ -2,24 +2,33 @@
 PDX Code Guild Bootcamp - Orchid
 lab 10: ROT Cipher'''
 
+import string
+
 plaintext_list = []
 plaintext = input("Please enter the phrase you would like to encrypt. \n")
+basic = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',)
+caps_list = [letter.capitalize() for letter in basic]
+
 for char in plaintext:
-    char = char.lower()
     plaintext_list.append(char)
 
-
-basic = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',)
-
-
-print(plaintext_list)
 encrypted_list = []
-for letter in (plaintext_list):
+for letter in plaintext_list:
     if letter in basic:
         i = basic.index(letter)
         cipher = basic[i+ 13]
-        print(cipher)
-        encrypted_list.append(cipher)
+        
+    
+    elif letter in caps_list:
+        i = caps_list.index(letter)
+        cipher = caps_list[i+13]
+        
+    
+    else: 
+        cipher = " "
+
+    encrypted_list.append(cipher)
+
 
 message = ""
 for item in encrypted_list:
