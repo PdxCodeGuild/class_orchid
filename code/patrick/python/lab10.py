503a43fdeda03ea94c853c2833f96c75f7334f90
patrick <patrickcotewurzler@yahoo.com>
2022-07-28 11:29:13 -0400

    lab10 with requested changes

--- code/patrick/python/lab10.py
+++ code/patrick/python/lab10.py
@@ -1,10 +1,10 @@
 
 
 
-cipher1 = "abcdefghijklmnopqrstuvwxyz"
+cipher1 = " abcdefghijklmnopqrstuvwxyz"
 
 
-cipher2 = "nopqrstuvwxyzabcdefghijklm"
+cipher2 = " nopqrstuvwxyzabcdefghijklm"
 
 
 
@@ -15,14 +15,14 @@ user_string = input(f"/nPlease enter a one word phrase: ")
 
 final_output = ""
 for letter in (user_string):
-    print(letter)
+    
     cor_num = cipher1.index(letter)
-    print(cor_num)
+    
     final_output += cipher2[cor_num]
     
   
 
-print(final_output)
+print("the coded messege is: ", final_output)
 
 
 
