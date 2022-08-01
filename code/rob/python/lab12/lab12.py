a541097e43b2b1e8b50932369bb62b9f748be40a
robrpete <rob.r.pete@gmail.com>
2022-08-01 12:49:07 -0400

    lab12 to csv

--- code/rob/python/lab12/lab12.py
+++ code/rob/python/lab12/lab12.py
@@ -13,14 +13,21 @@ for row in lines:
     i = 0
     items = row.split(',')
     dict_to_add = {}
-    while i < len(title_names):
+    
+    while i < len(items):
         dict_to_add.update({title_names[i]: items[i]})
         i += 1
     list_of_dicts.append(dict_to_add)
 
 def create_entry():
+    name = input('Enter a name: ')
+    for dict in list_of_dicts:
+        if name == dict['name']:
+            while name == dict['name']:
+                name = input('Name exist, please enter another name: ')
+                
     list_of_dicts.append({
-        'name': input('Enter a name: '),
+        'name': name,
         'role': input('Enter a role: '),
         'type': input('Enter a type: '),
         'skins': input('Enter the number of skins: '),})
@@ -75,28 +82,40 @@ update_csv = ''
 temp_keys = []
 temp_values = []
 
-for dict in list_of_dicts:
-    temp_v = []
-    for key, value in dict.items():
-        if key not in temp_keys:
-            temp_keys.append(key)
-        temp_v.append(value)
-    temp_values.append(temp_v)
-for t in temp_keys:
-    update_csv += t
-    if t != temp_keys[-1]:
-        update_csv += ','
-    elif t == temp_keys[-1]:
-            update_csv += '\n'
-for value in temp_values:
-    for item in value:
-        update_csv += item
-        if item != value[-1]:
-            update_csv += ','
-        elif item == value[-1]:
-            update_csv += '\n'
+def get_key_values():
+    for dict in list_of_dicts:
+        temp_v = []
+        for key, value in dict.items():
+            if key not in temp_keys:
+                temp_keys.append(key)
+            temp_v.append(value)
+        temp_values.append(temp_v)
+def keys_to_csv(csv):
+    for t in temp_keys:
+        csv += t
+        if t != temp_keys[-1]:
+            csv += ','
+        elif t == temp_keys[-1]:
+                csv += '\n'
+    return csv
+def values_to_csv(csv):
+    for value in temp_values:
+        for item in value:
+            csv += item
+            if item != value[-1]:
+                csv += ','
+            elif item == value[-1]:
+                if value != temp_values[-1]:
+                    csv += '\n'
+    return csv
 
-print(update_csv)
+get_key_values()
+update_csv = keys_to_csv(update_csv)
+update_csv = values_to_csv(update_csv)
+
+with open(file_path, 'w') as f:
+    f.write(update_csv)
+
 # name,role,type,skins
 # pele,jg,phys,9
 # thana,jg,phys,12
