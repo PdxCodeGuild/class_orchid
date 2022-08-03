76456b7a42115ac98fd6f03e916bfe93afadcea0
Tony <tony@webavant.com>
2022-08-02 20:28:05 -0700

    simpler math. trailing newline.

--- code/tony/python/classes/lab13version1.py
+++ code/tony/python/classes/lab13version1.py
@@ -1,19 +1,15 @@
-from decimal import Decimal, DefaultContext
-DefaultContext.rounding = 'ROUND_DOWN'
-round_exp = Decimal('.00')
-
 class ATM:
-    def __init__(self, balance=0, interest_rate=.01):
-        self.balance = Decimal(balance)
-        self.interest_rate = Decimal(interest_rate)
+    def __init__(self, balance=.0, interest_rate=.01):
+        self.balance = balance
+        self.interest_rate = interest_rate
 
     def check_balance(self):
         """return the account balance"""
-        return self.balance
+        return round(self.balance, 2)
 
     def deposit(self, amount):
         """deposit a given amount into account"""
-        amount = Decimal(amount).quantize(round_exp)
+        amount = round(amount, 2)
         self.balance += amount
         return amount
 
@@ -23,10 +19,11 @@ class ATM:
 
     def withdraw(self, amount):
         """withdraw given amount from account and return that amount"""
-        self.balance -= Decimal(amount).quantize(round_exp)
+        amount = round(amount, 2)
+        self.balance -= amount
         return amount
 
     def calc_interest(self):
         """calculate and return interest gained on account"""
-        interest = Decimal(self.interest_rate * self.balance).quantize(round_exp)
+        interest = round(self.interest_rate * self.balance, 2)
         return interest
