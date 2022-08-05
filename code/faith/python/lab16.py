b10c2cec48b93a54c99f00aa3a31d9ab87c2eef9
Faith Cicero <faithmates98@gmail.com>
2022-08-05 12:22:37 -0600

    lab16

---
+++ code/faith/python/lab16.py
@@ -0,0 +1,25 @@
+
+import requests
+import json
+
+
+search_term = input('please enter search term: ')
+params = {
+    'page':'1',
+    'limit':'20',
+    'term': search_term
+}
+
+
+
+response = requests.get('https://icanhazdadjoke.com/search',params=params, headers={'accept': 'application/json'})
+# print(response.text)
+joke_dict = response.json()
+joke = joke_dict['results']
+
+final_joke =joke[0]
+print(final_joke['joke'])
+
+
+
+   
