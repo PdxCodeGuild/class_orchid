f35b11a7bd5446b321ebb3adc497e311452c894c
Faith Cicero <faithmates98@gmail.com>
2022-08-09 16:18:41 -0600

    minicapstone

--- code/faith/python/OrderUp/OrderUp_app/admin.py
+++ code/faith/python/OrderUp/OrderUp_app/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-# admin.site.register(Order)
-# Register your models here.
-# from django.contrib import admin
-# from .models import Order 
-# admin.site.register(Order)
+from .models import Order
+admin.site.register(Order)
--- code/faith/python/OrderUp/OrderUp_app/models.py
+++ code/faith/python/OrderUp/OrderUp_app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Order(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
+    # entree = models.Choices()
+    # add_ons = models.Choices()
+    
     
 # order = Order.objects.get(id=1)
 # new_order = Order(first_name='first_name', last_name='last_name')
