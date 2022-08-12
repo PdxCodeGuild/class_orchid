ea2fa50dbee6e79093e3268e799c0283f664a7cc
patrick <patrickcotewurzler@yahoo.com>
2022-08-12 14:12:33 -0400

    lab16 with requested changes

--- code/patrick/python/lab16.py
+++ code/patrick/python/lab16.py
@@ -11,7 +11,7 @@ import json
 
     
 params = {
-    'results' : '20',
+    'limit' : '20',
     'term' : 'search_term',
     'inc' : 'joke',
 
@@ -19,7 +19,7 @@ params = {
 term = input("please enter your search term or quit: ")
 results = int(input(f"Please enter the number of results wanted: "))
 params.update({'term': term})
-params.update({'results': results})
+params.update({'limit': results})
 print(params)
 options = '''
     1. show me jokes
@@ -31,16 +31,16 @@ while True:
     jokes = []
     user_input = input(options)    
     if user_input == "1":
-        while len(jokes) <= int(results):
-            response = requests.get("https://icanhazdadjoke.com/search", params=params, headers = {'accept' : 'application/json'})
-            response = json.loads(response.text)
-            #print(response["joke"])
-            result = response["results"]
-            for i, item in enumerate(result):
-                w1 = result[i]
-                w2 = (w1['joke'])
+        
+        response = requests.get("https://icanhazdadjoke.com/search", params=params, headers = {'accept' : 'application/json'})
+        response = json.loads(response.text)
+        #print(response["joke"])
+        result = response["results"]
+        for i, item in enumerate(result):
+            w1 = result[i]
+            w2 = (w1['joke'])
                 
-                print(w2)
+            print(w2)
             
                     
 
