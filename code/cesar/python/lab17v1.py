067a2cd365d0930c51c9fba649493f5ef08b8989
Cesar Rebolledo <rebolledoces@gmail.com>
2022-08-12 13:01:11 -0400

    Lab17 Version 1 is now complete.

--- code/cesar/python/lab17v1.py
+++ code/cesar/python/lab17v1.py
@@ -1,8 +1,25 @@
-import requests, json, pprint 
+import requests, json
+from pprint import pprint
 
-api_key = '1073ce4606cd8c09f89ca186d81ab75f'
-response = requests.get('https://favqs.com/api/typeahead', headers={'accept':'application/json'})
+# api_key = '1073ce4606cd8c09f89ca186d81ab75f'
 
-# response = json.loads(response.text)
+"""
+Version 1: Get a Random Quote
+The URL to get a random quote is https://favqs.com/api/qotd, send a request here, parse the JSON 
+in the response into a python dictionary, and show the quote and the author.
 
-print(response)
+Cesar Rebolledo 
+"""
+
+
+response = requests.get(f'https://favqs.com/api/qotd')
+response = json.loads(response.text)
+print(f"""
+{"*"*40}
+Random quote generated just for you: 
+
+    "{response['quote']['body']}"
+
+Quote by: {response['quote']['author']}
+{"*"*40}
+""")
