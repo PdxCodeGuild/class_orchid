583de675a0f7406c2431355fdbe4c91b9cfac601
patrick <patrickcotewurzler@yahoo.com>
2022-08-15 13:30:11 -0400

    lab17ver2 in progress

--- code/patrick/python/lab17ver2.py
+++ code/patrick/python/lab17ver2.py
@@ -10,17 +10,31 @@ import pprint
 
 while True:
     params = {
-        "keyword": "keyword"
+        
+        "inc" : "author, body",
+        "results": "25"
 
         }
+   
+    
     keyword = input(f"enter the keyword or 'done' to quit: ")
-    params.update({"keyword" : keyword })
+    
+    headers = {
+    'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"',
+     'accept' : 'application/json',
+     }
+    
+    
     
     if keyword != "done":
-        #response = requests.get("https://favqs.com/api/quotes", params=params, headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'} )
-        response = requests.get("https://favqs.com/api/quotes?filter=<keyword>", params=params, headers = {'Content-Type: application/json''Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'} )
+        response = requests.get("https://favqs.com/api/quotes?filter=<keyword>", params=params, headers = headers )
+        #response = requests.get("https://favqs.com/api/quotes', params=params , headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'} )
+        #response = requests.get("https://favqs.com/api/quotes?filter=keyword", params=params,  headers = {'accept' : 'application/json'} )
+        print(response)
+        #response = json(response.text)
         response = response.json()
         #pprint.pprint(response.keys())
+        print(response)
         
         quotes = response['quotes']
         print(quotes)
