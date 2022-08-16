14d7b96e4944a727eb53416f6a3bfea6e1040e8b
patrick <patrickcotewurzler@yahoo.com>
2022-08-16 15:10:44 -0400

    lab16 with final revision

--- code/patrick/python/lab16.py
+++ code/patrick/python/lab16.py
@@ -10,26 +10,33 @@ import json
 
 
     
-params = {
+
+import time
+while True:
+    options = '''
+    1. show me jokes
+    2. changer search term
+    3. quit
+    '''
+    params = {
     'limit' : '20',
     'term' : 'search_term',
     'inc' : 'joke',
 
     }
-term = input("please enter your search term or quit: ")
-results = int(input(f"Please enter the number of results wanted: "))
-params.update({'term': term})
-params.update({'limit': results})
-print(params)
-options = '''
-    1. show me jokes
-    2. changer search term
-    3. quit
-'''
-import time
-while True:
+    term = input("please enter your search term: ")
+    results = int(input(f"Please enter the number of results wanted: "))
+    params.update({'term': term})
+    params.update({'limit': results})
+    print(params)
     jokes = []
     user_input = input(options)    
+    if user_input == "2":
+        search_term = input("please enter your search term: ")
+        params.update({'search_term': search_term})
+        if search_term == "quit":
+                    print(f"Game over!")
+                    break
     if user_input == "1":
         
         response = requests.get("https://icanhazdadjoke.com/search", params=params, headers = {'accept' : 'application/json'})
@@ -54,12 +61,6 @@ while True:
         print(f"Game over, Thanks for laughing with us!")
         break
     
-    if user_input == "2":
-        search_term = input("please enter your search term or quit: ")
-        params.update({'search_term': search_term})
-        if search_term == "quit":
-                    print(f"Game over!")
-                    break
 
 
 
