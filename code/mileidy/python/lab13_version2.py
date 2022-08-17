b39d9932aa7a384207f678f8c25bc72bd7e1438c
mileidy <mmileidy015@gmail.com>
2022-08-16 20:19:42 -0500

    updated lab 13

--- code/mileidy/python/lab13_version2.py
+++ code/mileidy/python/lab13_version2.py
@@ -7,6 +7,18 @@ class ATM:
         #starting interest rate
         self.log = []
         #where everything is stored
+        
+    def __reciept__(self,type_of_transaction,amount):
+        if type_of_transaction == 'deposit':
+            self.log.append(type_of_transaction)
+            self.log.append(amount)
+        elif type_of_transaction == 'withdraw':
+            self.log += type_of_transaction, amount
+        elif type_of_transaction == 'interest':
+            self.log.append(type_of_transaction)
+            self.log.append(amount)
+        else:
+            print('this code block doesnt work')
 
     def check_balance(self):
         #where i return account balance
@@ -34,7 +46,17 @@ class ATM:
         calc_interest = (self.balance * 0.1) / 100
         return calc_interest
 
-atm = ATM()  # create an instance of our class
+    def print_transaction(self):
+        trans = ''
+        for index in range(len(self.log)):
+            if index % 2 == 1:
+                trans += str(self.log[index]) + '\n'
+            else: 
+                trans += str(self.log[index]) + ' '
+                
+        return f'You have successfully \n{trans}'
+
+atm = ATM()  
 print('Hello and Welcome to the ATM! \n')
 print(f"-balance\n-deposit\n-withdraw\n-interest\n-help\n-exit\n")
 
@@ -42,23 +64,25 @@ while True:
     command = input('Enter a command: ').lower()
     # i .lowered the input so that the strings would match
     if command == 'balance':
-        balance = atm.check_balance()  # call the check_balance() method
+        balance = atm.check_balance() 
         print(f'Your current balance is ${balance}')
     elif command == 'deposit':
         amount = float(input('How much would you like to deposit? \n: '))
-        atm.deposit(amount)  # call the deposit(amount) method
+        atm.deposit(amount)
+        atm.__reciept__(type_of_transaction ='deposit',amount = amount)
         print(f'You have Deposited ${amount}')
     elif command == 'withdraw':
         amount = float(input('How much would you like to withdraw? \n: '))
-            # call the check_withdrawal(amount) method
         if atm.check_withdrawal(amount):
-            atm.withdraw(amount)  # call the withdraw(amount) method
+            atm.withdraw(amount) 
+            atm.__reciept__(type_of_transaction ='withdraw',amount = amount)
             print(f'You have successfully withdrawn ${amount}')
         else:
             print('Insufficient funds')
     elif command == 'interest':
-        amount = atm.calc_interest()  # call the calc_interest() method
+        amount = atm.calc_interest() 
         atm.deposit(amount)
+        atm.__reciept__(type_of_transaction ='interest',amount = amount)
         print(f'Accumulated ${amount} in interest')
     elif command == 'help':
         print('Available commands:')
@@ -68,8 +92,11 @@ while True:
         print('interest - accumulate interest')
         print('exit     - exit the program')
     elif command == 'exit':
+        copy_of_transaction = input('Would you like a reciept for your transaction? (yes/no)\n: ').lower()
+        if copy_of_transaction == 'yes':
+            print(atm.print_transaction())
+        elif copy_of_transaction == 'no':
+            print('Goodbye!')
         break
     else:
         print('command not recognized.')
-
-
