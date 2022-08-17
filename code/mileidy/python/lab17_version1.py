ee07ceb48c8d112a351c0368c3b1ef486df33dfb
mileidy <mmileidy015@gmail.com>
2022-08-17 14:31:21 -0500

    actual lab 17 complete

--- code/mileidy/python/lab17_version1.py
+++ code/mileidy/python/lab17_version1.py
@@ -1,3 +1,13 @@
-import api
 
-print(api)
+import requests
+from pprint import pprint
+#i referenced request.md
+
+response = requests.get('https://favqs.com/api/qotd/')
+# pprint(response.text)
+data = response.json()
+# pprint(data)
+
+
+print(data['quote']['author'] + '\n' + data['quote']['body'])
+
