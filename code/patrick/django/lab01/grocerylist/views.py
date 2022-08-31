61d6ebfe556c1a3ccbb286da8a49d4d09609813b
patrick <patrickcotewurzler@yahoo.com>
2022-08-31 13:19:27 -0400

    lab01 in orogress

--- code/patrick/django/lab01/grocerylist/views.py
+++ code/patrick/django/lab01/grocerylist/views.py
@@ -1,7 +1,20 @@
+from re import template
 from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import groceryitem
+
+from django.template import loader
+"""
+def index(request):
+    full_grocery_list = groceryitem.objects
+    template = loader.get_template("grocerylist/index.html")
+    context = {full_grocery_list: full_grocery_list,}
+    return HttpResponse(template.render(context, request))
+"""
 
 def index(request):
-    return HttpResponse("grocery list")
+    full_grocery_list = groceryitem.objects.order_by('-pub_date')[:5]
+    context = {'full_grocery_list': full_grocery_list, }
+    return render(request, 'grocerylist/index.html', context)
--- code/patrick/django/lab01/mysite/urls.py
+++ code/patrick/django/lab01/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('grocerylist.urls')),
+    path("grocerylist/", include('grocerylist.urls')),
     path('admin/', admin.site.urls),
 ]
