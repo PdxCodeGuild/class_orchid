00055cadfdf6449bd079b1300f7f23fd936f398a
mileidy <mmileidy015@gmail.com>
2022-08-31 12:28:49 -0500

    lab00

--- code/mileidy/DJANGO/lab_00/mysite/polls/views.py
+++ code/mileidy/DJANGO/lab_00/mysite/polls/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
+from . models import Question
 # Create your views here.
 def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list' : latest_question_list
+    }
+    return render(request, 'polls/index.html',context)
 
-    return HttpResponse["<h1>Hello World</h1>"]
+
+def detail(request,question_id):
+    return HttpResponse(f"You're looking at question{question_id}")
+
+def results(request,question_id):
+    response = f"You're looking at the results of {question_id}"
+    return HttpResponse(response)
+
+def vote(request,question_id):
+    return HttpResponse (f"You're voting on question {question_id}")
