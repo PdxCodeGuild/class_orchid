ac4908598ef53b6c90686198e98103025041df0e
Faith Cicero <faithmates98@gmail.com>
2022-09-08 12:32:53 -0600

    logic for submit view

--- code/faith/Django/URLShortener/urlshort_app/views.py
+++ code/faith/Django/URLShortener/urlshort_app/views.py
@@ -1,23 +1,33 @@
 
+from gc import get_objects
 from multiprocessing import context
-from django.shortcuts import render, redirect
+from random import choice
+from string import ascii_letters
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
 from .models import UrlShort
 
 def index(request):
-    long_url = UrlShort.objects.all(long_url=long_url)
-    context = {
-        'long_url':long_url
-        }
-    return render(request, 'urlshort_app/index.html', context )
-
-def submit_url(request):
-    if request.method == 'POST':
-        long_url = request.POST.get('url')
+    if request.method =="POST":
+        long_url = request.POST('long_url')
+        # short_url = submit_url()
         UrlShort.objects.create(long_url=long_url)
-    return redirect('/')
+        return render(request, 'urlshort_app/index.html')
 
-    
+
+# def submit_url(request):
+#     if request.method == 'POST':
+#         long_url = request.POST.get('url')
+#         UrlShort.objects.create(long_url=long_url)
+#     return redirect('/')
+
+def submit_url():
+    return ''.join([choice(ascii_letters)for x in range (8)])
+
+def redirect():
+    new_url = get_object_or_404(UrlShort)
+    return HttpResponseRedirect(new_url.long_url)
+
 
 
 
