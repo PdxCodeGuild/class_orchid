ab856dccc6e6c2d6399601c2e513f5db74a787fd
Cesar Rebolledo <rebolledoces@gmail.com>
2022-09-15 12:59:37 -0400

    adding to remove

--- code/cesar/django/lab_03/chirp/chirp/urls.py
+++ code/cesar/django/lab_03/chirp/chirp/urls.py
@@ -21,6 +21,8 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('users.urls')),
+    path('users/', include('users.url')),
+    path('users/', include('django.contrib.auth.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('posts/', include('posts.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
