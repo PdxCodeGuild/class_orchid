30fc079520ea417bc2c2b7739415fec5acdd0ee6
Cesar Rebolledo <rebolledoces@gmail.com>
2022-09-15 15:11:06 -0400

    stashed files I did not need on this branch.

---
+++ code/cesar/js/lab_02/lab_02.js
@@ -0,0 +1,16 @@
+// let data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+
+// function peakFinder(data) {
+//     let peakList = []
+//     for (let i = 0; i )
+// }
+
+// program to display text 5 times
+const n = 10;
+
+for (let i = 0; i <= n; i++) {
+    console.log(`I love JavaScript.`);
+}
+
+
+
