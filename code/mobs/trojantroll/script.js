e4c6b500969e4622abdc941082be8679adb92068
JordynHolder <Jordyn.W.Holder@gmail.com>
2022-09-16 11:47:31 -0700

    Jordyn's turn ended

--- code/mobs/trojantroll/script.js
+++ code/mobs/trojantroll/script.js
@@ -4,10 +4,16 @@ let addBtn = document.getElementById('addbutton')
 
 let addItemText = document.getElementById('additem')
 
+let todoList = document.getElementById('todolist')
 
 console.log(addBtn)
 addBtn.addEventListener('click', e => {
-    let item = { value: addItemText.value(), isComplete: false }
+    let item = { value: addItemText.value, isComplete: false }
     todo.push(item)
 
+    var li = document.createElement("li")
+    li.innerText = item.value
+    todoList.appendChild(li)
+
+    console.log(todo)
 })
