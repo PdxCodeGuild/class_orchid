aca2a0930e5d2d5271e986ed908975d69256c585
Cesar Rebolledo <rebolledoces@gmail.com>
2022-09-16 14:13:06 -0400

    completed drive, move to Jordyn

--- code/mobs/trojantroll/script.js
+++ code/mobs/trojantroll/script.js
@@ -1,1 +1,13 @@
 let todo = []
+
+let addBtn = document.getElementById('addbutton')
+
+let addItemText = document.getElementById('additem')
+
+
+console.log(addBtn)
+addBtn.addEventListener('click', e => {
+    let item = { value: addItemText.value(), isComplete: false }
+    todo.push(item)
+
+})
