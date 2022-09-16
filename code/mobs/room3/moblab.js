58337665900d42e9ecc13dc2cdeda7056e0566b3
Velshaun <vellbyers@gmail.com>
2022-09-16 13:12:51 -0500

    added html outline with notes attached

---
+++ code/mobs/room3/moblab.js
@@ -0,0 +1,7 @@
+// gonna a prompt for user input (text input field)
+// array to store users input (push to list)
+// display the array on the page (completed and add)
+// adding a remove button (event listener with a click)
+// add a complete button (event listener with a click)
+
+
