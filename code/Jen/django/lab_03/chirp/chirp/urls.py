95ec45fe4024b073c96b4f4376559e5d8872b104
NSJen <109029525+NSJen@users.noreply.github.com>
2022-09-19 18:23:09 -0400

    Jen/django/lab03 (#294)
    * Django lab 2
    
    * Django lab 3 added
    
    Co-authored-by: Gage Lieble <96758346+Gage-Lieble@users.noreply.github.com>
    Co-authored-by: Danny Burrow <burrowingdan@gmail.com>

--- code/Jen/django/lab_03/chirp/chirp/urls.py
+++ code/Jen/django/lab_03/chirp/chirp/urls.py
@@ -1,4 +1,4 @@
-"""url_shortener URL Configuration
+"""chirp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,12 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
-import shortening.urls
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', include(shortening.urls)),
-    path('admin/', admin.site.urls),
+    path('posts/', include('posts.urls'), name = "posts"),
+    path('admin/', admin.site.urls),     
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include("django.contrib.auth.urls")),
+    path('', TemplateView.as_view(template_name="home.html"), name = "home"),
+    
 
 ]
 
