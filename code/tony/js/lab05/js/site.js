119481176b44f9f4f39c611b508bcd0567eefe92
Tony <tony@webavant.com>
2022-09-20 16:14:17 -0700

    fix sort and improve ui

--- code/tony/js/lab05/js/site.js
+++ code/tony/js/lab05/js/site.js
@@ -1,20 +1,35 @@
 const App = {
     el: '#app',
     data: () => {
-        return { todos: [] }
+        return { todos: [
+            { task: 'Example one', complete: false, },
+            { task: 'Example two', complete: false, },
+            { task: 'Example three', complete: true, },
+            { task: 'Example four', complete: false, },
+            { task: 'Example five', complete: true, },
+            { task: 'Example six', complete: false, },
+            { task: 'Example seven', complete: false, },
+        ] }
     },
     methods: {
         addTodo(e) {
-            this.todos.push({task:e.target.value,complete:false})
+            this.todos.push({
+                task: e.target.value,
+                complete:false,
+            })
             localStorage.todos = JSON.stringify(this.todos)
         },
         completeTodo(index) {
-            this.todos[index].complete = this.todos[index].complete ? false : true
+            this.todos[index].complete = !this.todos[index].complete
             localStorage.todos = JSON.stringify(this.todos)
         },
         deleteTodo(index) {
             this.todos.splice(index, 1)
             localStorage.todos = JSON.stringify(this.todos)
+        },
+        clearTodos() {
+            localStorage.clear()
+            this.todos = []
         },
     },
     mounted() {
@@ -22,6 +37,10 @@ const App = {
             this.todos = JSON.parse(localStorage.todos)
         }
     },
+    updated() {
+            this.todos.sort((a,b) => a.complete < b.complete ? -1 : 1 )
+    },
+    computed: {}
 }
 
 const app = Vue.createApp(App)
