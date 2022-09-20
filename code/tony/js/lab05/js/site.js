819c007d278d5ab24a77d2d196b1c2cb93485855
Tony <tony@webavant.com>
2022-09-20 11:03:55 -0700

    js lab05

--- code/tony/js/lab05/js/site.js
+++ code/tony/js/lab05/js/site.js
@@ -1,7 +1,26 @@
 const App = {
     el: '#app',
     data: () => {
-        return { message: 'Hello, world!' }
+        return { todos: [] }
+    },
+    methods: {
+        addTodo(e) {
+            this.todos.push({task:e.target.value,complete:false})
+            localStorage.todos = JSON.stringify(this.todos)
+        },
+        completeTodo(index) {
+            this.todos[index].complete = this.todos[index].complete ? false : true
+            localStorage.todos = JSON.stringify(this.todos)
+        },
+        deleteTodo(index) {
+            this.todos.splice(index, 1)
+            localStorage.todos = JSON.stringify(this.todos)
+        },
+    },
+    mounted() {
+        if (localStorage.todos) {
+            this.todos = JSON.parse(localStorage.todos)
+        }
     },
 }
 
