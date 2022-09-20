2042211c6d8c78300032fb197505ae60772ac2df
mileidy <mmileidy015@gmail.com>
2022-09-20 11:39:40 -0500

    need to work out math bugs

--- code/mileidy/javascript/lab_03/average_numbers.js
+++ code/mileidy/javascript/lab_03/average_numbers.js
@@ -4,10 +4,16 @@ let nums = []
 while (true) {
     let choice = prompt('Please enter a number or enter done to get results')
     if (choice == 'done'){
-        let length = nums.length 
-        let average = d3.sum(nums) / length
-        alert(`Your number average is: ${average}`)
-    nums.push(choice)
-        break
-    }
-}
+        //code block doesnt work
+//         let length = nums.length 
+//         for (const value of nums) {
+//             sum += value;
+//         }
+//         let average = value / length
+//         alert(`Your number average is: ${average}`)
+//         nums.push(choice)
+//         break
+//     }
+// }
+
+// need to figure out how to do the math portion for the average number
