e23f9411b05a6a9b446d20ca6d9a825098c95670
Faith Cicero <faithmates98@gmail.com>
2022-09-20 16:37:26 -0600

    lab02b actually done

--- code/faith/JS/lab02/lab02b.js
+++ code/faith/JS/lab02/lab02b.js
@@ -8,44 +8,45 @@ enter.addEventListener('click', function(){
 
     
     if (unit == 'ft'){
-        result = distance * 0.3048
+        storeMath = 0.3048
     }
     else if (unit == 'mi') {
-        result = distance * 1609.34
+        storeMath = 1609.34
     }
     else if (unit == 'm') {
-        result = distance
+        storeMath = 1
     }
     else if (unit == 'km') {
-        result = distance * 1000
+        storeMath = 1000
     }
     else if (unit == 'yard') {
-        result = distance * 0.9144
+        storeMath = 0.9144
     }
     else if (unit == 'inch') {
-        result = distance * 0.0254
+        storeMath = 0.0254
     }
     
     if (unit2 == 'ft'){
-        result/0.3048
+        storeMath2 = 0.3048
     }
     else if (unit2 == 'mi'){
-        result/1609.34
+        storeMath2 = 1609.34
     }
     else if (unit2 == 'm'){
-        result 
+        storeMath2 = 1
     }
     else if (unit2 == 'km'){
-        result/1000
+        storeMath2 = 1000
     }
     else if (unit2 == 'yard'){
-        result/0.9144
+        storeMath2 = 0.9144
     }
     else if (unit2 == 'inch'){
-        result/0.0254
+        storeMath2 = 0.0254
     }
+    let conversion = distance * storeMath
+    let finalCon = conversion/storeMath2
+    results.innerHTML = finalCon
     
-    results.innerHTML = result
-    console.log(result)
 }
 )
