174ab350f268211cdad01fc649bb1541dd6e5689
mileidy <mmileidy015@gmail.com>
2022-09-20 15:51:17 -0500

    lab_03 javascript

--- code/mileidy/javascript/lab_03/average_numbers.js
+++ code/mileidy/javascript/lab_03/average_numbers.js
@@ -4,16 +4,12 @@ let nums = []
 while (true) {
     let choice = prompt('Please enter a number or enter done to get results')
     if (choice == 'done'){
-        //code block doesnt work
-//         let length = nums.length 
-//         for (const value of nums) {
-//             sum += value;
-//         }
-//         let average = value / length
-//         alert(`Your number average is: ${average}`)
-//         nums.push(choice)
-//         break
-//     }
-// }
-
-// need to figure out how to do the math portion for the average number
+        break
+    }else if (choice != 'done'){
+        nums.push(parseInt(choice))
+    }
+}
+let length = nums.length ;
+let sum = nums.reduce((x, y)=> x + y)
+let average = sum / length;
+alert(`Your number average is: ${average}`)
