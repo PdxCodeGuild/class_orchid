537a0f6f4ade3c2a3c485bd34a8709aa95b89cdf
Cesar Rebolledo <rebolledoces@gmail.com>
2022-09-21 15:05:47 -0400

    part a done

--- code/cesar/js/lab_02/script_a.js
+++ code/cesar/js/lab_02/script_a.js
@@ -1,16 +1,12 @@
-//  Lab 2 Javascript - Python Lab 05 - Black Jack
+// unit converter - js lab 
 
-const deck = { "0": 0, "A": 11, "10": 10, "J": 10, "Q": 10, "K": 10, }
+const units = { "ft": 0.3048, "mi": 1609.34, "m": 1, "km": 1000, "yd": 0.9144, "in": 0.0254 }
 
-let card1 = prompt('What is your first card? ').toUpperCase()
-let card2 = prompt('What is your second card? ').toUpperCase()
-let card3 = prompt('What is your third card? ').toUpperCase()
+const userInputNumDistance = prompt('what is the distance you would like to convert?')
+const userInputUnits = prompt('what is the distance you would like to convert?ft, mi, m, km, yd, or in ')
+const userOutputUnits = prompt('what is the distance you would like to convertft, mi, m, km, yd, or in ')
 
-console.log(card1, card2, card3)
+const metersTotal = userInputNumDistance * units[userInputUnits]
+const result = metersTotal / units[userOutputUnits]
 
-const total = card1 + card2 + card3
-
-console.log(total)
-
-
-
+alert(Math.ceil(result) + " " + userOutputUnits)
