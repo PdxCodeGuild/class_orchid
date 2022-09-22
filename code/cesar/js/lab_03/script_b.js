17a416660c187dd4584a4d9f2b96065202b4ef07
Cesar Rebolledo <rebolledoces@gmail.com>
2022-09-22 17:44:37 -0400

    part b is done and ready for grading

--- code/cesar/js/lab_03/script_b.js
+++ code/cesar/js/lab_03/script_b.js
@@ -1,20 +1,15 @@
 //  Lab3 - CC Validator Part A
 // The credit card number to check is: 4556737586899855 i
 
-let cardNum = document.getElementById('cardnum')
+// let cardNum = document.getElementById('cardnum')
 let submit = document.getElementById('submit')
-let submitClick = submit.addEventListener('click', cardNum.value)
 
-alert(submitClick.value)
-
-
-
-
-
-function creditCardValidator(cardNum) {
+function cardNumArray() {
+    const cardNum = document.getElementById('cardnum').value
     const arrayOfCardNum = Array.from(String(cardNum), Number)
     const checkDigit = arrayOfCardNum.pop()
     const reverseArrayOfCardNum = arrayOfCardNum.reverse()
+
     for (const [index, element] of reverseArrayOfCardNum.entries()) {
         if (index % 2 == 0) {
             let dubElement = reverseArrayOfCardNum[index] = element * 2
@@ -29,9 +24,40 @@ function creditCardValidator(cardNum) {
         return accumulator + value
     }, 0)
     const finalCheckDigit = sumOfArray % 10
+
     if (finalCheckDigit == checkDigit) {
-        return true
+        return document.getElementById('result').innerHTML = "Valid"
     } else {
-        return false
+        return document.getElementById('result').innerHTML = "Invalid"
     }
 }
+
+
+
+
+
+
+// function creditCardValidator(cardNum) {
+//     const arrayOfCardNum = Array.from(String(cardNum), Number)
+//     const checkDigit = arrayOfCardNum.pop()
+//     const reverseArrayOfCardNum = arrayOfCardNum.reverse()
+//     for (const [index, element] of reverseArrayOfCardNum.entries()) {
+//         if (index % 2 == 0) {
+//             let dubElement = reverseArrayOfCardNum[index] = element * 2
+//         }
+//     }
+//     for (const [index, element] of reverseArrayOfCardNum.entries()) {
+//         if (element > 9) {
+//             let greaterThanNineElement = reverseArrayOfCardNum[index] = element - 9
+//         }
+//     }
+//     const sumOfArray = reverseArrayOfCardNum.reduce((accumulator, value) => {
+//         return accumulator + value
+//     }, 0)
+//     const finalCheckDigit = sumOfArray % 10
+//     if (finalCheckDigit == checkDigit) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
