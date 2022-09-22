830a52138c628594e405d1409880922f39788c17
Faith Cicero <faithmates98@gmail.com>
2022-09-22 15:55:03 -0600

    lab04 complete

--- code/faith/JS/todos/js/site.js
+++ code/faith/JS/todos/js/site.js
@@ -1,13 +1,33 @@
-
-console.log('hello')
 const App = ({
     el: '#app',
     data: ()=> {
         return{
-            todo:'',
-            
+            todo: [],
+            complete: [],
         }
-    }
+    },
+    methods: {
+        addTodo: function () {
+            this.todo.push(this.todo.item)
+            this.todo.item = ''
+        },
+        deleteTodo: function (item) {
+            this.todo.splice(this.todo.indexOf(item),1)
+            // this.complete.splice(this.todo.indexOf(item),1)
+        },
+        completeTodo: function(item) {
+            this.complete.push(item)
+            this.todo.splice(this.todo.indexOf(item),1)
+        },
+        undoTodo: function(item) {
+            this.todo.push(item)
+            this.complete.splice(this.complete.indexOf(item),1)
+
+        },
+        deleteComplete: function (item) {
+            this.complete.splice(this.complete.indexOf(item),1)
+        }
+        }
 })
 
 const app = Vue.createApp(App)
