670eef60d343eb9b43959adc92f3f94f017f1e32
Danny Burrow <danny@pdxcodeguild.com>
2022-09-23 15:46:54 -0700

    remove copied files

--- code/jordyn/faith/Django/URLShortener/urlshort_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'urlshort_app' # for namespacing
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:short_url>', views.url_redirect, name='url_redirect')
-]
