f0c11b7602094b74ae83f4091fe723814ce7a948
teyoncooper <nov.energii@github.com>
2022-09-26 18:20:25 -0500

    capstone

--- code/teyon/javascript/capstone/capstone.js
+++ code/teyon/javascript/capstone/capstone.js
@@ -4,7 +4,10 @@ new Vue({
     el: '#app',
     data () {
         return {
-            info: null
+            info: null,
+            incoming: null, 
+            outgoing: null,
+            date: null,
         }
     },
     methods: {
@@ -13,7 +16,7 @@ new Vue({
             
             axios
             .get('https://priceline-com-provider.p.rapidapi.com/v1/flights/search',
-            params = {"itinerary_type":"ONE_WAY","class_type":"ECO","location_arrival":arrival_iata,"date_departure":date,"location_departure":outgoing_iata,"sort_order":"PRICE","number_of_stops":"1","price_max":"20000","number_of_passengers":"1","duration_max":"2051","price_min":"100"},
+            params = {"itinerary_type":"ONE_WAY","class_type":"ECO","location_arrival":arrival_iata,"date_departure":this.date,"location_departure":outgoing_iata,"sort_order":"PRICE","number_of_stops":"1","price_max":"20000","number_of_passengers":"1","duration_max":"2051","price_min":"100"},
             headers = {
                 'X-RapidAPI-Key': '71c7cd9e87msh636c8b829338161p16fe2djsnac414d19ae23',
                 'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
