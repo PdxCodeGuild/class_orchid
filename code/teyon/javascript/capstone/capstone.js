8c0aa981326d1ac9b61377fe200a7a87bd1faa52
teyoncooper <nov.energii@github.com>
2022-09-27 18:17:13 -0500

    capstone

--- code/teyon/javascript/capstone/capstone.js
+++ code/teyon/javascript/capstone/capstone.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import airportData from "./airports.js"
 
 new Vue({
     el: '#app',
@@ -8,22 +8,56 @@ new Vue({
             incoming: null, 
             outgoing: null,
             date: null,
+            airports: airportData,
+            outgoingIata: null,
+            incomingIata: null,
         }
     },
     methods: {
-        flightdata(){
-            
-            
+
+        getIata(){
+            let departure = this.outgoing
+
+            for(let key in this.airports){
+                if(this.airports[key]["location"] == departure){
+                    this.outgoingIata = this.airports[key]["airport"]
+                }
+                
+            }
+            let arrival = this.incoming
+
+            for(let key in this.airports){
+                if(this.airports[key]["location"] == arrival){
+                    this.incomingIata = this.airports[key]["airport"]
+
+                }
+            }
+
+            this.flightData()
+        },
+
+        flightData(){
             axios
-            .get('https://priceline-com-provider.p.rapidapi.com/v1/flights/search',
-            params = {"itinerary_type":"ONE_WAY","class_type":"ECO","location_arrival":arrival_iata,"date_departure":this.date,"location_departure":outgoing_iata,"sort_order":"PRICE","number_of_stops":"1","price_max":"20000","number_of_passengers":"1","duration_max":"2051","price_min":"100"},
-            headers = {
+            .get('https://priceline-com-provider.p.rapidapi.com/v1/flights/search',{
+            params: {"itinerary_type":"ONE_WAY","class_type":"ECO","location_arrival":this.incomingIata,"date_departure":this.date,"location_departure":this.outgoingIata,"sort_order":"PRICE","number_of_stops":"1","price_max":"20000","number_of_passengers":"1","duration_max":"2051","price_min":"100"},
+            headers: {
                 'X-RapidAPI-Key': '71c7cd9e87msh636c8b829338161p16fe2djsnac414d19ae23',
                 'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
             }
+        }
             )
-            .then(response => (this.info = response.data.flights))
+            .then(response => (this.info = response))
 
             }
+    },
+
+    computed: {
+        filteredInfo(){
+
+
+
+
+        }
+
     }
 })
