69d1890a4af7f459e2d944f8f6041dce55c182b9
mileidy <mmileidy015@gmail.com>
2022-10-04 14:10:08 -0500

    lab 06

--- code/mileidy/javascript/lab_06/javascript/app.js
+++ code/mileidy/javascript/lab_06/javascript/app.js
@@ -1,13 +1,33 @@
 const App = {
     el: '#app',
     data: () => {
-        return { message: 'Hello, world!', amazon : "" }
+        return { data : [] , searchKey : "", page : 1 , searchType : ""}
     },
     mounted () {
         axios
         .get('https://favqs.com/api/quotes/', {headers : {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}})
-        .then(response => (console.log(response)))
-    }
+        .then(response => (this.data = response.data.quotes))
+    },
+    methods :{
+        filter(s) {
+            axios
+        .get('https://favqs.com/api/quotes/', {headers : {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}, params : {"filter" : this.searchKey, "page" : this.page, "type": this.searchType}})
+        .then(response => (this.data = response.data.quotes))
+        },
+        pagination() {
+            this.page += 1
+            axios
+        .get('https://favqs.com/api/quotes/', {headers : {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}, params : {"filter" : this.searchKey, "page" : this.page }})
+        .then(response => (this.data = response.data.quotes))
+        },
+        flip() {
+            this.page -= 1
+            axios
+            .get('https://favqs.com/api/quotes/', {headers : {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}, params : {"filter" : this.searchKey, "page" : this.page }})
+            .then(response => (this.data = response.data.quotes))
+        }
+    },
+    
 }
 
 const app = Vue.createApp(App)
