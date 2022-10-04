a1816acf5c2cc7977db449f56e7479be6200ec6d
Cesar Rebolledo <rebolledoces@gmail.com>
2022-10-04 13:25:50 -0400

    working MVP

--- code/cesar/js/lab_06/app.js
+++ code/cesar/js/lab_06/app.js
@@ -2,10 +2,11 @@ new Vue({
     el: '#app',
     data: () => {
         return {
-            info: null,
-            searchTerm: '',
             page: 1,
             type: '',
+            output: null,
+            searchTerm: '',
+
         }
     },
     mounted (){
@@ -14,15 +15,13 @@ new Vue({
                 headers: {'Authorization':'Token token="855df50978dc9afd6bf86579913c9f8b"'}, 
             })
             .then(response => { 
-                (this.info = (this.info = response.data.quotes))
+                (this.output = (this.output = response.data.quotes))
             })
-            console.log(this.info)
+            console.log(this.output)
     },
     methods: {
         search(e) {
-
             this.searchTerm = e.target.value
-
             axios
                 .get('https://favqs.com/api/quotes', {
                     headers: {'Authorization':'Token token="855df50978dc9afd6bf86579913c9f8b"'}, 
@@ -32,7 +31,7 @@ new Vue({
                 }
                 })
                 .then(response => { 
-                    (this.info = response.data.quotes)
+                    (this.output = response.data.quotes)
 
 
                 })
@@ -47,9 +46,7 @@ new Vue({
             }
         })
             .then(response => { 
-                (this.info = response.data.quotes)
-
-
+                (this.output = response.data.quotes)
         })
     },
 
