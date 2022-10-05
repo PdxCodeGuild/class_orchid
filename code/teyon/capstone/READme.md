5137a0e4a9ef3551d5c7bf99b2c575c949d122b7
teyoncooper <nov.energii@github.com>
2022-10-05 17:53:20 -0500

    teyon capstone

---
+++ code/teyon/capstone/READme.md
@@ -0,0 +1,47 @@
+# small adventures x BIG MEMORIES
+
+### PDX CodeGuild Final Capstone Project By Teyon Cooper
+search flights to locations, and retrieve information about your incoming location, along with pictures, add a rating or review along with flight pricing.
+
+## Project Outline
+
+small adventures x BIG MEMORIES at its core is a flight finder. Although the main function is finding flight, it differs from other travel apps with more detailed information about the arriving destination. Using the Priceline.com API for searching flights in conjunction with Radar API which offers the ability to produce pictures and information about locations, the user the chance to see what to expect prior to further looking into purchasing a ticket.The search results returns flights and next to the flights images, and detailed information about the location. Creating an account allows your to rate and leave a comment on a location that you may have previously visited.
+
+## Features
+Features of the app include the ability to search using simply a city for outgoing and incoming, and retrieve flight information along with information about the incoming location of choice. pictures and details about the location.
+
+###### Primary Functions
+1. Flight Searchable
+2. Information on incoming location
+3. Images of incoming location
+4. Ability to leave comment on location
+5. Ability to like location
+
+## Data Model
+1. Search bars for incoming and outgoing data. 
+2. Display available tickets 
+3. Next to available tickets display images and 
+
+
+## Schedule
+###### Week 1 - MVP
+1. Find alternative to IATA issue so that all options for airports are called
+2. Get API calls to Radar's geotargeting abilities
+3. Have the information called from Priceline interact with the api call to radar
+4. Ensure images and information is available
+5. Create rating system
+6. Create comment system
+
+##### Week 2 - Views
+1. Create the views that display the functioning code 
+2. Create sign in and sign up pages
+3. Create pages that display the results
+4. Create pages that display comments with location
+
+##### Week 3 - Finalizing
+1. Start the process of css and styling views
+2. Show images of popular places on the page where search occurs
+3. Navigation bars
+4. Deploy app prior to presentation
+
+1. AIzaSyAQR4tvBFNHhw9GgWy16Db2hh-GriSCTAw
---
+++ code/teyon/capstone/adventure.js
@@ -0,0 +1,207 @@
+import airportData from "./airports.js"
+
+new Vue({
+    el: '#app',
+    data () {
+        return {
+            info: null,
+            incoming: null, 
+            outgoing: null,
+            date: null,
+            airports: airportData,
+            outgoingIata: null,
+            incomingIata: null,
+            favFlight: [],
+            incLat: null,
+            incLon: null,
+            outLat: null,
+            outLon: null,
+            price: null,
+            map: null,
+            information: null
+        }
+    },
+
+    methods: {
+
+        getIata(){
+            let departure = this.outgoing
+
+            for(let key in this.airports){
+                if(this.airports[key]["location"] == departure){
+                    this.outgoingIata = this.airports[key]["airport"]
+                }
+                
+            }
+            let arrival = this.incoming
+
+            for(let key in this.airports){
+                if(this.airports[key]["location"] == arrival){
+                    this.incomingIata = this.airports[key]["airport"]
+
+                }
+            }
+
+            this.flightData()
+            
+        },
+
+        flightData(){
+            axios
+            .get('https://priceline-com-provider.p.rapidapi.com/v1/flights/search',{
+            params: {"itinerary_type":"ONE_WAY","class_type":"ECO","location_arrival":this.incomingIata,"date_departure":this.date,"location_departure":this.outgoingIata,"sort_order":"PRICE","number_of_stops":"1","price_max":"20000","number_of_passengers":"1","duration_max":"2051","price_min":"100"},
+            headers: {
+                'X-RapidAPI-Key': '71c7cd9e87msh636c8b829338161p16fe2djsnac414d19ae23',
+                'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
+            }
+        }
+            )
+            .then(response => { 
+                for (let index = 0; index < response.data.totalTripSummary.airport.length; index++){
+                    if(response.data.totalTripSummary.airport[index].lowestTotalFare.amount == response.data.totalTripSummary.minTotalFareWithTaxesAndFees){
+                        this.price = response.data.totalTripSummary.airport[index]
+                        console.log(this.price)
+                        }
+                    }
+                })
+        
+                    
+
+            this.iataLatLong()
+            
+
+            },
+
+        savedFlights(item){
+
+            this.favFlight.push(item)
+            
+
+
+        },
+
+        iataLatLong(){
+            let arriving = this.incomingIata
+            
+            // axios
+            // .get ('https://priceline-com-provider.p.rapidapi.com/v1/flights/locations',{
+            // params: {name: arriving},
+            // headers: {
+            //     'X-RapidAPI-Key': '71c7cd9e87msh636c8b829338161p16fe2djsnac414d19ae23',
+            //     'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
+            // }
+            // }
+            // )
+            // .then(response => {
+            //     this.incLatLon = response.data[id]
+            //     console.log(this.incLatLon)})
+
+            // let depart = this.outgoingIata
+            
+            // axios
+            // .get ('https://priceline-com-provider.p.rapidapi.com/v1/flights/locations',{
+            // params: {name: depart},
+            // headers: {
+            //     'X-RapidAPI-Key': '71c7cd9e87msh636c8b829338161p16fe2djsnac414d19ae23',
+            //     'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
+            // }
+            // }
+            // )
+            // .then(response => {
+            //     this.outLatLon = response.data
+            //     console.log(this.outLatLon)
+            // })
+            axios
+            .get(`https://travel-hacking-tool.p.rapidapi.com/api/airports/${arriving}/`,{headers: {
+                'X-RapidAPI-Key': '71c7cd9e87msh636c8b829338161p16fe2djsnac414d19ae23',
+                'X-RapidAPI-Host': 'travel-hacking-tool.p.rapidapi.com'
+            }
+            })
+            .then(response => {
+                this.incLat = response.data.latitude
+                this.incLon = response.data.longitude
+                console.log(this.information)
+                console.log(this.incLat, "incLat")
+                console.log(this.incLon, "incLon")})
+                
+                let depart = this.outgoingIata
+
+                axios
+                .get(`https://travel-hacking-tool.p.rapidapi.com/api/airports/${depart}/`,{headers: {
+                    'X-RapidAPI-Key': '71c7cd9e87msh636c8b829338161p16fe2djsnac414d19ae23',
+                    'X-RapidAPI-Host': 'travel-hacking-tool.p.rapidapi.com'
+                }
+                })
+                .then(response => {
+                    this.outLat = response.data.latitude
+                    this.outLon = response.data.longitude
+                    this.information = response.data
+                    console.log(this.information)
+                    console.log(this.outLat, "outlat")
+                    console.log(this.outLon, "outlon")
+                    this.googleMap()    
+                    })
+                    
+                
+                },
+
+            googleMap(){
+
+                const outLatLon = { lat: this.outLat, lng: this.outLon };
+                const incLatLon = { lat:this.incLat, lng: this.incLon }
+                console.log(outLatLon)
+                console.log(this.outLat)
+                
+                
+                const map = new google.maps.Map(document.getElementById("map"), {
+                    center: { lat: 0, lng: 0 },
+                    zoom: 2,
+                });
+                new google.maps.Marker({
+                    position: outLatLon,
+                    map,
+                    title: "Hello World!",
+            });   
+
+                new google.maps.Marker({
+                position: incLatLon,
+                map,
+                title: "Hello World!",
+            });   
+            
+            },
+
+        },
+
+
+
+    computed: {
+        filteredInfo(){
+            if(this.price){
+                console.log(this.price)
+
+
+                return this.price
+                
+                
+
+            }
+        return null
+
+
+        }
+
+    },
+
+    mounted() {
+
+        const map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: 0, lng: 0 },
+            zoom: 2,
+        });
+
+    
+    }
+})
+
+
