810d4b338beff4e07f8c6682da0e7c9f8a75915f
Cesar Rebolledo <rebolledoces@gmail.com>
2022-10-11 16:53:16 -0400

    models has been updated

--- code/cesar/capstone/runlogr/posts/models.py
+++ code/cesar/capstone/runlogr/posts/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Chirps(models.Model):
+class Log(models.Model):
     chirp = models.CharField(max_length=128)
     created_date = models.DateTimeField(default=timezone.now)
     auth_users = models.ForeignKey(
