b78d5e5366c9ba63e456eca3d4785750a5b4261f
Velshaun <vellbyers@gmail.com>
2022-10-18 13:21:19 -0500

    daily commit

--- code/vel/capstone/config/settings.py
+++ code/vel/capstone/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'exercise',
     'users',
+    'planner',
 ]
 
 MIDDLEWARE = [
--- code/vel/capstone/planner/models.py
+++ code/vel/capstone/planner/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from datetime import datetime, timedelta
 from django.contrib.auth import get_user_model
+from django.forms import CharField
 
-# Create your models here.
+class Planner(models.Model):
+    title = models.CharField(max_length = 200)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    user = models.OneToOneField(get_user_model(),on_delete=models.CASCADE, related_name='user_planner')
