057d54ee6253199e6f32058bd9e44595d2235393
robrpete <rob.r.pete@gmail.com>
2022-10-19 01:53:42 -0400

    random

--- code/rob/capstone/capstone/smite/views.py
+++ code/rob/capstone/capstone/smite/views.py
@@ -14,30 +14,32 @@ auth_key = data['AUTH_KEY']
 date_utc = datetime.utcnow()
 date = date_utc.strftime('%Y%m%d%H%M%S')
 
+
 def index(request):
     session_id = Session.objects.get(getter_id=1)
     print('sess id', session_id)
     sig = f'{dev_id}testsession{auth_key}{date}'
     sig_hashed = hashlib.md5(sig.encode()).hexdigest()
-    test_response = requests.get(f'https://api.smitegame.com/smiteapi.svc/testsessionjson/{dev_id}/{sig_hashed}/{session_id}/{date}').json()
-    
-    if(test_response.startswith('Invalid session id')):
+    test_response = requests.get(
+        f'https://api.smitegame.com/smiteapi.svc/testsessionjson/{dev_id}/{sig_hashed}/{session_id}/{date}').json()
+
+    if (test_response.startswith('Invalid session id')):
         print('created session')
         sig = f'{dev_id}createsession{auth_key}{date}'
         sig_hashed = hashlib.md5(sig.encode()).hexdigest()
-        response = requests.get(f'https://api.smitegame.com/smiteapi.svc/createsessionjson/{dev_id}/{sig_hashed}/{date}').json()
+        response = requests.get(
+            f'https://api.smitegame.com/smiteapi.svc/createsessionjson/{dev_id}/{sig_hashed}/{date}').json()
         Session.objects.all().delete()
-        sess = Session(getter_id=1,session_id=response['session_id'])
+        sess = Session(getter_id=1, session_id=response['session_id'])
         sess.save()
         print(sess)
-        
+
     else:
         response = test_response
-    
-    
+
     print(test_response, response)
-    context = {'dev': dev_id, 'auth': auth_key, 
-        'hashed': sig_hashed, 'time': date, 'res': response, 'sess': session_id}
+    context = {'dev': dev_id, 'auth': auth_key,
+               'hashed': sig_hashed, 'time': date, 'res': response, 'sess': session_id}
     return render(request, 'smite/index.html', context)
 
 
@@ -45,7 +47,8 @@ def gods(request):
     session_id = Session.objects.get(getter_id=1)
     sig = f'{dev_id}getgods{auth_key}{date}'
     sig_hashed = hashlib.md5(sig.encode()).hexdigest()
-    response = requests.get(f'https://api.smitegame.com/smiteapi.svc/getgodsjson/{dev_id}/{sig_hashed}/{session_id}/{date}/1').json()
+    response = requests.get(
+        f'https://api.smitegame.com/smiteapi.svc/getgodsjson/{dev_id}/{sig_hashed}/{session_id}/{date}/1').json()
     context = {'res': response, 'sess': session_id}
     return render(request, 'smite/gods.html', context)
 
@@ -54,7 +57,8 @@ def items(request):
     session_id = Session.objects.get(getter_id=1)
     sig = f'{dev_id}getitems{auth_key}{date}'
     sig_hashed = hashlib.md5(sig.encode()).hexdigest()
-    response = requests.get(f'https://api.smitegame.com/smiteapi.svc/getitemsjson/{dev_id}/{sig_hashed}/{session_id}/{date}/1').json()
+    response = requests.get(
+        f'https://api.smitegame.com/smiteapi.svc/getitemsjson/{dev_id}/{sig_hashed}/{session_id}/{date}/1').json()
     context = {'res': response, 'sess': session_id}
     return render(request, 'smite/items.html', context)
 
@@ -67,7 +71,8 @@ def god(request, r_id):
     session_id = Session.objects.get(getter_id=1)
     sig = f'{dev_id}getgods{auth_key}{date}'
     sig_hashed = hashlib.md5(sig.encode()).hexdigest()
-    response = requests.get(f'https://api.smitegame.com/smiteapi.svc/getgodsjson/{dev_id}/{sig_hashed}/{session_id}/{date}/1').json()
+    response = requests.get(
+        f'https://api.smitegame.com/smiteapi.svc/getgodsjson/{dev_id}/{sig_hashed}/{session_id}/{date}/1').json()
     god = {}
     for res in response:
         if res['id'] == r_id:
