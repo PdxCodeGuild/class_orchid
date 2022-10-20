167b76c63056c7ae7e2c895d8053dcc6fcde01fb
teyoncooper <34337433+teyoncooper@users.noreply.github.com>
2022-10-20 14:29:52 -0500

    Teyon/javascript/lab06 (#365)
    * lab03 complete
    
    * lab06
    
    * lab06
    
    * lab06
    
    * lab06
    
    * lab06 complete
    
    * updated with tag option added
    
    * fix overwrite
    
    Co-authored-by: teyoncooper <nov.energii@github.com>
    Co-authored-by: Danny Burrow <danny@pdxcodeguild.com>
    Co-authored-by: Danny Burrow <burrowingdan@gmail.com>

---
+++ code/teyon/javascript/lab06.js
@@ -0,0 +1,54 @@
+new Vue({
+
+    el: '#app',
+    data: () => {
+        return{
+            info: null,
+            pages: 1,
+            find: '',
+            type: '',
+
+        }
+        
+    },
+    methods: {
+        typedInput(input) {
+            this.find = input.target.value
+
+        axios
+            .get('https://favqs.com/api/quotes', {
+                headers: {'Authorization':'Token token="b739c98330b6c5c4d8d49a60dd6543e4"'},
+                params: {'filter': this.find, 'pages': this.pages, 'type': this.type}
+            })
+            .then(response => {
+                (this.info = response.data.quotes)
+        })
+        console.log(this.info)
+        console.log(this.page)
+        console.log(this.type)
+    },
+    next(){
+        this.pages += 1
+        axios
+        .get('https://favqs.com/api/quotes', {
+            headers: {'Authorization': 'Token token="b739c98330b6c5c4d8d49a60dd6543e4"', 'Access-Control-Allow-Origin': '*'},
+            params: {'filter': this.find, 'page': this.pages}
+        })
+        .then(response => {
+            (this.info = response.data.quotes)
+        })
+    }
+
+    },
+
+    mounted (){
+
+        axios
+            .get('https://favqs.com/api/quotes', {
+                headers:{'Authorization': 'Token token="b739c98330b6c5c4d8d49a60dd6543e4"', 'Access-Control-Allow-Origin': '*'}
+            })
+            .then(response => {
+                (this.info = (this.info = response.data.quotes))
+        })
+    }
+})
